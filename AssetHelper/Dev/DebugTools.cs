2a59bf7647f50f3f5c5081813e28e4d96e544247
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-12-06 21:24:17 +0000

    Add dump all asset names

--- AssetHelper/Dev/DebugTools.cs
+++ AssetHelper/Dev/DebugTools.cs
@@ -1,4 +1,12 @@
-﻿using System.IO;
+﻿using BepInEx.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using NameListLookup = System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<string>>;
 
 namespace Silksong.AssetHelper.Dev;
 
@@ -7,6 +15,8 @@ namespace Silksong.AssetHelper.Dev;
 /// </summary>
 public static class DebugTools
 {
+    private static readonly ManualLogSource Log = Logger.CreateLogSource($"AssetHelper.{nameof(DebugTools)}");
+
     private static DirectoryInfo AssemblyFolder => Directory.GetParent(typeof(DebugTools).Assembly.Location);
 
     /// <summary>
@@ -21,7 +31,49 @@ public static class DebugTools
         AssetsData.InvokeAfterAddressablesLoaded(() => AssetsData.BundleKeys.SerializeToFileInBackground(dumpFile));
     }
 
-    // TODO - dump asset names in bundle
-    // TODO - dump all asset names in all bundles (slow). Also types
-    // TODO - dump asset key -> asset type in Addressables
+    /// <summary>
+    /// Dump all asset names to the asset_names.json file next to this assembly.
+    /// 
+    /// This function loads all asset bundles, so is very slow.
+    /// </summary>
+    public static void DumpAllAssetNames()
+    {
+        AssetsData.InvokeAfterAddressablesLoaded(DumpAllAssetNamesInternal);
+    }
+
+    private static void DumpAllAssetNamesInternal()
+    {
+        string dumpFile = Path.Combine(AssemblyFolder.FullName, "asset_names.json");
+
+        NameListLookup assetNames = [];
+        NameListLookup sceneNames = [];
+        
+        Stopwatch sw = Stopwatch.StartNew();
+        foreach ((string name, string key) in AssetsData.BundleKeys!)
+        {
+            IAssetBundleResource rsc = Addressables.LoadAssetAsync<IAssetBundleResource>(key).WaitForCompletion();
+            AssetBundle b = rsc.GetAssetBundle();
+
+            string[] bundleScenePaths = b.GetAllScenePaths();
+            if (bundleScenePaths.Length > 0)
+            {
+                sceneNames[name] = bundleScenePaths.ToList();
+            }
+            string[] bundleAssetNames = b.GetAllAssetNames();
+            if (bundleAssetNames.Length > 0)
+            {
+                assetNames[name] = bundleAssetNames.ToList();
+            }
+        }
+        sw.Stop();
+        Log.LogInfo($"Determined asset names in {sw.ElapsedMilliseconds} ms");
+
+        Dictionary<string, NameListLookup> data = new()
+        {
+            ["assets"] = assetNames,
+            ["scenes"] = sceneNames,
+        };
+
+        data.SerializeToFileInBackground(dumpFile);
+    }
 }
