92280754b7ea1b2415e59295d1d33025fc1061bd
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-12-22 22:17:50 +0000

    Add handle to undo gameplay asset

--- AssetHelper/LoadedAssets/LoadableAssetExtensions.cs
+++ AssetHelper/LoadedAssets/LoadableAssetExtensions.cs
@@ -1,18 +1,45 @@
-﻿namespace Silksong.AssetHelper.LoadedAssets;
+﻿using System;
+
+namespace Silksong.AssetHelper.LoadedAssets;
 
 /// <summary>
 /// Extension methods to control when Loadable Assets are loaded.
 /// </summary>
 public static class LoadableAssetExtensions
 {
+    private class GameplayAssetHandle<T>(LoadableAsset<T> asset) : IDisposable where T : UObject
+    {
+        private bool isDisposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    GameEvents.OnEnterGame -= asset.DoLoad;
+                }
+                isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
     /// <summary>
     /// Ensure that this asset is always loaded while in-game.
     /// 
     /// If currently in game, will load the asset.
     /// </summary>
     /// <param name="asset">The asset to set.</param>
+    /// <param name="handle">Dispose the handle to undo the SetGameplayAsset operation.</param>
     public static LoadableAsset<T> SetGameplayAsset<T>(
-        this LoadableAsset<T> asset)
+        this LoadableAsset<T> asset,
+        out IDisposable handle)
         where T : UObject
     {
         GameEvents.OnEnterGame += asset.DoLoad;
@@ -21,6 +48,8 @@ public static class LoadableAssetExtensions
         {
             asset.Load();
         }
+
+        handle = new GameplayAssetHandle<T>(asset);
 
         return asset;
     }
