f8943f8c556c6487ccdd8432981a7f5bbf8ca191
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-01-05 19:16:26 +0000

    Add in patch hook for AssetsTools

--- AssetHelper/AssetHelperPlugin.cs
+++ AssetHelper/AssetHelperPlugin.cs
@@ -1,4 +1,5 @@
 using AssetsTools.NET;
+using AssetsTools.NET.Extra;
 using BepInEx;
 using BepInEx.Logging;
 using MonoMod.Cil;
@@ -6,8 +7,11 @@ using MonoMod.RuntimeDetour;
 using Silksong.AssetHelper.BundleTools;
 using Silksong.AssetHelper.Internal;
 using Silksong.AssetHelper.Plugin;
+using System;
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Silksong.AssetHelper;
 
@@ -23,6 +27,7 @@ public partial class AssetHelperPlugin : BaseUnityPlugin
     private static readonly Dictionary<string, string> Keys = [];
 
     private ILHook _atHook;
+    private Hook _atChook;
 
     private void Awake()
     {
@@ -37,9 +42,29 @@ public partial class AssetHelperPlugin : BaseUnityPlugin
 
         // TODO - remove this when assetstools.net gets updated
         _atHook = new ILHook(typeof(AssetTypeValueIterator).GetMethod(nameof(AssetTypeValueIterator.ReadNext)), PatchATVI);
+        _atChook = new Hook(typeof(Net35Polyfill).GetMethod(nameof(Net35Polyfill.CopyToCompat)), PatchC2C);
 
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
     }
+
+    private void PatchC2C(Action<Stream, Stream, long, int> orig, Stream input, Stream output, long bytes, int bufferSize)
+    {
+        byte[] buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+        int read;
+
+        // set to largest value so we always go over buffer (hopefully)
+        if (bytes == -1)
+            bytes = long.MaxValue;
+
+        // bufferSize will always be an int so if bytes is larger, it's also under the size of an int
+        while (bytes > 0 && (read = input.Read(buffer, 0, (int)Math.Min(buffer.Length, bytes))) > 0)
+        {
+            output.Write(buffer, 0, read);
+            bytes -= read;
+        }
+        ArrayPool<byte>.Shared.Return(buffer);
+    }
+
 
     /// <summary>
     /// Fixes a bug with AssetTypeValueIterator where it moves 4 bytes forward when reading a double rather than 8
