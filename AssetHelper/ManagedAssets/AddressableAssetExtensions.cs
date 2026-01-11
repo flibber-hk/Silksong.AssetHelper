de9c20c360c069d56989dc5134de22555a1ad216
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-01-11 10:05:15 +0000

    Add InstantiateAsset extension

--- AssetHelper/ManagedAssets/AddressableAssetExtensions.cs
+++ AssetHelper/ManagedAssets/AddressableAssetExtensions.cs
@@ -19,4 +19,17 @@ public static class AddressableAssetExtensions
 
         return UObject.Instantiate(asset.Handle.Result);
     }
+
+    /// <summary>
+    /// Instantiate an asset in this group accessed by key.
+    /// </summary>
+    public static T InstantiateAsset<T>(this AddressableAssetGroup<T> group, string key) where T : UObject
+    {
+        if (!group.IsLoaded)
+        {
+            throw new InvalidOperationException($"The group has not finished loading!");
+        }
+
+        return UObject.Instantiate(group[key].Result);
+    }
 }
